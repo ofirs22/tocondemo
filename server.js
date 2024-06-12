723aa66fd53e20787c4f253cc51fbdb2cb943aa7
ofirs22 <75007980+ofirs22@users.noreply.github.com>
2024-06-13 00:22:49 +0300

    port from env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors')
 const app = express();
 
-const PORT = 3038;
+const PORT = process.env.PORT || 3038;
 app.use(cors('*'));
 app.use(express.json());
 
